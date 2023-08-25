1022684b347deee43cb7b582d5748a155486dc2d
Burhan Uddin <bunnub5683@gmail.com>
2023-08-25 11:14:13 +0600

    authentication api done

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,12 +1,29 @@
 // according to interface mongoose model goes here
+//schema
 import { Schema, model } from 'mongoose';
-import { IUser } from './user.interface';
+import { UserType } from './user.interface';
 
-const userSchema = new Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  image: { type: String },
+const userSchema = new Schema<UserType>({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password:{
+    type: String,
+    required: true
+  },
+  mobile: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String
+  },
 });
 
 // 3. Create a Model.
-export const User = model<IUser>('User', userSchema);
+export const UserModel = model<UserType>('User', userSchema);
